ebc58843901f9ccd9d7320ace5dafed13f63fec3
Alan Wong <naganowl@users.noreply.github.com>
2019-02-07 13:36:57 -0800

    Make blacklisted classes configurable
    Allow the selectors to be passed in as an array so that they can be configured to the needs of a given codebase

--- helpers/blacklisted-class.js
+++ helpers/blacklisted-class.js
@@ -1,35 +1,7 @@
 const collectClasses = require('./collect-classes');
 
-const ACTION_CLASSES = require('./action-classes');
-const CONTROLLER_CLASSES = require('./controller-classes');
-
-const BLACKLIST_CLASSES = [
-  '.backgrid',
-  '.backgrid-paginator',
-  '.category',
-  '.disabled',
-  '.editable',
-  '.has-items',
-  '.item',
-  '.mjs-nestedSortable-error',
-  '.renderable',
-  '.sort-caret',
-  '.sortable',
-  '.string-cell',
-  '.username',
-].concat([
-  ...CONTROLLER_CLASSES,
-  ...ACTION_CLASSES,
-]);
-const BLACKLIST_PREFIXES = [
-  '.language-',
-  '.select2',
-  '.selectize',
-  '.tdcss',
-  '.tddcss',
-  '.tipsy',
-  '.ui-',
-];
+let BLACKLIST_CLASSES;
+let BLACKLIST_PREFIXES;
 
 function blacklistedPrefix(classList) {
   // Loop over `classList` top level in order to return an offending selector.
@@ -44,12 +16,18 @@ function blacklistedPrefix(classList) {
   Given a full (possibly) hierarchical selector, return the blacklisted class
   that matches or the full selector with a blacklisted prefix
 */
-module.exports = function blacklistedClass(selectorName) {
-  const selectorClasses = collectClasses(selectorName).map((selector) => {
-    return `.${selector}`;
-  });
-  const hasPrefix = blacklistedPrefix(selectorClasses);
-  return hasPrefix || BLACKLIST_CLASSES.find((className) => {
-    return selectorClasses.includes(className);
-  });
+module.exports = {
+  init: function(opts) {
+    BLACKLIST_CLASSES = opts.BLACKLIST_CLASSES;
+    BLACKLIST_PREFIXES = opts.BLACKLIST_PREFIXES;
+  },
+  find: function(selectorName) {
+    const selectorClasses = collectClasses(selectorName).map((selector) => {
+      return `.${selector}`;
+    });
+    const hasPrefix = blacklistedPrefix(selectorClasses);
+    return hasPrefix || BLACKLIST_CLASSES.find((className) => {
+      return selectorClasses.includes(className);
+    });
+  },
 };
