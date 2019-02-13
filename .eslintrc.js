0f5fcd4d95f8b2f06189088493739f316f87f660
Alan Wong <naganowl@users.noreply.github.com>
2019-02-12 19:38:33 -0800

    Allow testing dependencies to be imported for spec files

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,9 @@ module.exports = {
   extends: 'airbnb',
   rules: {
     'implicit-arrow-linebreak': 'off',
+    'import/no-extraneous-dependencies': [
+      'off',
+      {'devDependencies': ['**/*.spec.js']
+    }],
   },
 };
